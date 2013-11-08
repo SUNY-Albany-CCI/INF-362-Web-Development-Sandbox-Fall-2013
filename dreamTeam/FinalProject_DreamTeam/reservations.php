243ad43f0089846382e7ce7a4399c5274b6f3457
Mari Johannessen <mari.l.johannessen@gmail.com>
2013-11-08 00:30:05 +0000

    Changed the menu structure of all the pages.

--- dreamTeam/FinalProject_DreamTeam/reservations.php
+++ dreamTeam/FinalProject_DreamTeam/reservations.php
@@ -30,10 +30,11 @@
 					<h1><a href="index.html" id="logo">Cajun Restaurant</a></h1>
 					<nav>
 						<ul id="menu">
-							<li><a href="index.php"><img src="images/home_button.png" alt=""></a></li>
-							<li class="active"><a href="reservations.php"><img src="images/reservation_button.png" alt=""></a></li>
-							<li><a href="order_online.php"><img src="images/order_button.png" alt=""></a></li>
-							<li><a href="contact.php"><img src="images/contact_button.png" alt=""></a></li>
+							<li><a href="index.php"><p>Home</p></a></li>
+							<li><a href="order_online.php"><p>Menu</p></a></li>
+							<li class="active"><a href="reservations.php"><p>Make Reservation</p></a></li>
+							<li><a href="order_online.php"><p>Order Online</p></a></li>
+							<li><a href="contact.php"><p>Contact us</p></a></li>
 						</ul>
 					</nav>
 				</header>
@@ -68,8 +69,9 @@
 
 
 <body>
-<h1>Reservations</h1>
-
+<h1>Make Reservation</h1>
+<br />
+<br />
 <form action="finalPro.php" method="post">
 
 <table align="center">
