43a97ea3f1526910dfbe4fae485ce6eceb7a0bfd
Jarel Turner <jturner@ip-10-179-38-212.ec2.internal>
2013-11-21 21:37:35 +0000

    Adding contactPost.php, finalProject.php, contact.php, and finalPro.php

--- dreamTeam/FinalProject_DreamTeam/contact.php
+++ dreamTeam/FinalProject_DreamTeam/contact.php
@@ -1,91 +1,41 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Ahnvee - The best cajun in Albany</title>
-<meta charset="utf-8">
-<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
-<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
-<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<html>
+<head></head>
+<title></title>
 
-<script type="text/javascript" src="js/jquery-1.6.js" ></script>
-<script type="text/javascript" src="js/cufon-yui.js"></script>
-<script type="text/javascript" src="js/cufon-replace.js"></script>  
-<script type="text/javascript" src="js/Forum_400.font.js"></script>
-<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="js/tms-0.3.js"></script>
-<script type="text/javascript" src="js/tms_presets.js"></script>
-<script type="text/javascript" src="js/script.js"></script>
-<script type="text/javascript" src="js/atooltip.jquery.js"></script> 
 
-</head>
+<body>
 
-<body id="page1">
-<div class="body6">
-	<div class="body1">
-		<div class="body5">
-			<div class="main">
-			
-<!-- header -->
-				<header>
-					<h1><a href="index.html" id="logo">Cajun Restaurant</a></h1>
-					<nav>
-						<ul id="menu">
-							<li><a href="index.php"><p>Home</p></a></li>
-							<li><a href="order_online.php"><p>Menu</p></a></li>
-							<li><a href="reservations.php"><p>Make Reservation</p></a></li>
-							<li><a href="order_online.php"><p>Order Online</p></a></li>
-							<li class="active"><a href="contact.php"><p>Contact Us</p></a></li>
-						</ul>
-					</nav>
-				</header>
-<!-- / header -->
+<h1>Contact</h1>
 
-<!-- content -->
-				<article id="content">
-					<div class="content_bg">
-						
-						<div id="contact_form">
-						<!-- CONTACT FORM -->
-						<!-- The code for the contact form goes here -->
-						</div>
-						
-					</div>
-					<div class="wrap">
-						
-					</div>
-				</article>
-			</div>
-		</div>
-	</div>
-</div>
+<form action="contactPost" method="post">
 
-<div class="body3">
-	<div class="body4">
-		<div class="main">
-		
-<!-- footer -->
-			<footer>
-				<div class="wrapper">
-					<section class="col1 pad_left1">
-						<h3>Toll Free: <span>1-800 123 45 67</span></h3>
-					</section>
-					<section class="col2 pad_left1">
-						<h3>Follow Us </h3>
-						<ul id="icons">
-							<li><a href="#" class="normaltip" title="Facebook"><img src="images/icon1.gif" alt=""></a></li>
-							<li><a href="#" class="normaltip" title="Linkedin"><img src="images/icon2.gif" alt=""></a></li>
-							<li><a href="#" class="normaltip" title="Twitter"><img src="images/icon3.gif" alt=""></a></li>
-							<li><a href="#" class="normaltip" title="Delicious"><img src="images/icon4.gif" alt=""></a></li>
-							<li><a href="#" class="normaltip" title="Technorati"><img src="images/icon5.gif" alt=""></a></li>
-						</ul>
-					</section>
-				</div>
-			</footer>
-<!-- / footer -->
+<table align="center">
+<tr>
+<td>First Name: <input title="Enter your first name" type="text" name="fName" size="30" placeholder="First" required /></td>
+</tr>
 
-		</div>
-	</div>
-</div>
-<script type="text/javascript"> Cufon.now(); </script>
+<tr>
+<td>Last Name: <input title="Enter your last name" type="text" name="lName" size="30" placeholder="Last" required/></td>
+</tr>
+
+<tr>
+<td>Phone: <input title="Enter your phone number" type="tel" name="Phone" size="12" placeholder="(000)555-5555" required/></td>
+</tr>
+
+<tr>
+<td>Email: <input title="Enter your email" type="email" name="Email" size="20" placeholder="johndoe@email.com" required/></td>
+</tr>
+
+<tr>
+<td><textarea rows="4" cols="50" type="text" name="Comment" placeholder="Enter text here"></textarea></td>
+</tr>
+
+<tr>
+<td><input type="submit" value="Reserve"/></td>
+</tr>
+</form>
+
+
 </body>
 </html>
--- dreamTeam/FinalProject_DreamTeam/finalPro.php
+++ dreamTeam/FinalProject_DreamTeam/finalPro.php
@@ -7,14 +7,15 @@ $db = mysql_select_db("test",$conn);
 <?php
 
 $fname = ($_POST['fName']);
-//$phone = ($_POST['Phone']);
-//$seats = ($_POST['Seats']);
+$lname = ($_POST['lName']);
+$phone = ($_POST['Phone']);
+$section = ($_POST['Section']);
+$seats = ($_POST['Seats']);
 $date = ($_POST['Date']);
 $time = ($_POST['Time']);
 $sql = "INSERT into reservation(res_Fname, res_Lname, res_Phone, email, seats, date, time, tlbSect) 
 Values('$_POST[fName]','$_POST[lName]','$_POST[Phone]','$_POST[Email]','$_POST[Seats]','$_POST[Date]','$_POST[Time]','$_POST[Section]')";
 $qury = mysql_query($sql);
-
 #INSERT into reservations values
 
 if(!$qury)
@@ -23,9 +24,25 @@ else
     echo "Thank you. Your reservation was an success!";
     echo "<br>";
     echo "Your reservation is for " .$date. " at " .$time. "pm<br/>";
-    echo '<a href="/~Jarel/finalProject.php">Reservations Page</a>';
+    echo '<a href="#">Reservations Page</a>';
     echo "<br>";
-    echo '<a href="/~Jarel/finalProject.php">Home Page</a>';   
-     
+    echo '<a href="#">Home Page</a>';   
+  
+  
+  //email
+$email = ($_POST['Email']);
+$myemail= "jarelt.41@gmail.com";
+$email1 = "$email, $myemail";
+$subject = "Your reservation";
 
+$message = "
+Name: $fname $lname
+Date: $date
+Time: $time 
+Section: $section
+Seats: $seats
+Phone Number: $phone
+";
+//email   
+mail($email1,$subject,$message)
 ?>
