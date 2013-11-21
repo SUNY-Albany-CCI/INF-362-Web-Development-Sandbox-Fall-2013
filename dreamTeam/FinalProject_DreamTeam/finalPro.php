be69a586e322c3a843ce4eeb7578d183ebe8a10a
Mari Johannessen <mari.l.johannessen@gmail.com>
2013-11-21 21:50:17 +0000

    Made the confirmation page look like the other pages.

--- dreamTeam/FinalProject_DreamTeam/finalPro.php
+++ dreamTeam/FinalProject_DreamTeam/finalPro.php
@@ -1,3 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Ahnvee - The best cajun in Albany</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+<script type="text/javascript" src="js/jquery-1.6.js" ></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>  
+<script type="text/javascript" src="js/Forum_400.font.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/tms-0.3.js"></script>
+<script type="text/javascript" src="js/tms_presets.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/atooltip.jquery.js"></script> 
+
+</head>
+
+<body id="page1">
+<div class="body6">
+	<div class="body1">
+		<div class="body5">
+			<div class="main">
+			
+<!-- header -->
+				<header>
+					<h1><a href="index.html" id="logo">Cajun Restaurant</a></h1>
+					<nav>
+						<ul id="menu">
+							<li><a href="index.php"><p>Home</p></a></li>
+							<li><a href="order_online.php"><p>Menu</p></a></li>
+							<li class="active"><a href="reservations.php"><p>Make Reservation</p></a></li>
+							<li><a href="order_online.php"><p>Order Online</p></a></li>
+							<li><a href="contact.php"><p>Contact us</p></a></li>
+						</ul>
+					</nav>
+				</header>
+<!-- / header -->
+
+<!-- content -->
+				<article id="content">
+					<div class="content_bg">
+						
+<div id="make_reservation">
+
+						<!-- RESERVATIONS -->
+					
+					
+					
+					
+					
+					
+					
+					
+					
+												
+<!DOCTYPE html>
+
+<html>
+<!-- <link rel="stylesheet" href="finalPro.css" type="text/css"> -->
+<head>
+<script type="text/javascript" src="finalPro.js"></script>
+<title>Reservations</title>
+</head>
+
+
+
+<body>
+<div id="reservationContent">
+<h1 id="reservationsHeading">Thank you</h1>
+<br />
+<br />
+<br />
+<div id="confirmation">
 <?php
 
 $conn = mysql_connect('localhost','user1','user1pass');
@@ -23,10 +100,7 @@ if(!$qury)
 else
     echo "Thank you. Your reservation was an success!";
     echo "<br>";
-    echo "Your reservation is for " .$date. " at " .$time. "pm<br/>";
-    echo '<a href="#">Reservations Page</a>';
-    echo "<br>";
-    echo '<a href="#">Home Page</a>';   
+    echo "Your reservation is for " .$date. " at " .$time. "pm.<br/>";  
   
   
   //email
@@ -46,3 +120,62 @@ Phone Number: $phone
 //email   
 mail($email1,$subject,$message)
 ?>
+</div>
+</div>
+</body>
+
+</html>
+							
+							
+							
+							
+							
+							
+							
+							
+													
+						<!-- The code for making reservations goes here -->
+						
+						</div>
+						
+					</div>
+					<div class="wrap">
+						
+					</div>
+				</article>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="body3">
+	<div class="body4">
+		<div class="main">
+		
+<!-- footer -->
+			<footer>
+				<div class="wrapper">
+					<section class="col1 pad_left1">
+						<h3>Toll Free: <span>1-800 123 45 67</span></h3>
+					</section>
+					<section class="col2 pad_left1">
+						<h3>Follow Us </h3>
+						<ul id="icons">
+							<li><a href="#" class="normaltip" title="Facebook"><img src="images/icon1.gif" alt=""></a></li>
+							<li><a href="#" class="normaltip" title="Linkedin"><img src="images/icon2.gif" alt=""></a></li>
+							<li><a href="#" class="normaltip" title="Twitter"><img src="images/icon3.gif" alt=""></a></li>
+							<li><a href="#" class="normaltip" title="Delicious"><img src="images/icon4.gif" alt=""></a></li>
+							<li><a href="#" class="normaltip" title="Technorati"><img src="images/icon5.gif" alt=""></a></li>
+						</ul>
+					</section>
+				</div>
+			</footer>
+<!-- / footer -->
+
+		</div>
+	</div>
+</div>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
+
